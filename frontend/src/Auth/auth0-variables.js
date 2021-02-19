054b8d7d1c8ecad0cdf78fa607019c97df8f23e0
Vinesh S Talpankar <69723722+Vinesh-T@users.noreply.github.com>
2021-02-19 21:34:40 +0530

    Update auth0-variables.js

--- frontend/src/Auth/auth0-variables.js
+++ frontend/src/Auth/auth0-variables.js
@@ -1,6 +1,6 @@
 export const AUTH_CONFIG = {
-  domain: 'yourauth0-domain.auth0.com',
-  clientId: 'your-auth0-clientId',
+  domain: 'dev-y0t9vh0x.us.auth0.com',
+  clientId: 'ikajFdAIyznPyU82dtPeg5t4qypT9Vr0',
   callbackUrl: 'http://localhost:3001/callback', // you need set call back url at Auth0
-  audience: 'your-audience'
+  audience: 'https://dev-y0t9vh0x.us.auth0.com/api/v2/'
 }
